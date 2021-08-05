cb8b080996c2ac0707501d630aef1a85412320b6
Zion Brown <zabs95@gmail.com>
2021-08-04 23:05:52 -0400

    Added some cards and a holder for the cards on the menu page *stolen and tweaked from sound-community

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -3,18 +3,11 @@ import "./style.css";
 
 const Card = props => (
   <div className="card">
-    <div className="img-container" onClick={() => props.soundSetup(props.sound)} /* When you click a card, call soundSetup() in browse.js*/ >
+    
       <img alt={props.name} src={props.image}/>
-      <div className="soundName">{props.name}</div>
-    </div>
-    <div>
-      <div className="content">
-      <div  className="soundDate">Uploaded: {props.uploadDate}</div>
-       <div  className="soundUploader">Uploaded by: {props.user}</div> 
-         <a href={props.sound} download={props.name}>Download .{props.fileType}</a>
-         {/* Wont change name because not of same origin, must use javascript solution */}
-      </div>
-    </div>
+      <div className="cardName">{props.name}</div>
+      
+    
   </div>
 );
 
--- client/src/pages/Blog.js
+++ client/src/pages/Blog.js
@@ -1,10 +1,37 @@
 import Wrapper from "../components/Wrapper";
 import Title from "../components/Title"
+import Scroll from "../components/Scroll"
+import Card from "../components/Card"
 import React from "react";
 function Blog() {
     return (
         <Wrapper>
-        <Title>hello</Title>
+        <Title>Hello, welcome to Project Foresight.
+            <br></br>
+            Select an option from below to get started.
+        </Title>
+        <Scroll>
+        <Card
+        name={"Food"}
+        image="assets/images/hamburger-icon.png"
+        > 
+        </Card>
+        <Card
+        name={"Household"}
+        image="assets/images/toothpaste-icon.png"
+        > 
+        </Card>
+        <Card
+        name={"Food"}
+        image="assets/images/hamburger-icon.png"
+        > 
+        </Card>
+        <Card
+        name={"Food"}
+        image="assets/images/hamburger-icon.png"
+        > 
+        </Card>
+        </Scroll>
         </Wrapper>
     );
 }
